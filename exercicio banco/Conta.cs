4e8a2ff84a4c2422ee45eb81e914ef46fc244851
Osmar <osmar570@outlook.com>
2020-04-07 04:25:57 -0300

    primeiro commit do programa de banco

---
+++ exercicio banco/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_banco
+{
+    class Conta
+    {
+        private string _nome;
+        public int NumeroConta { get; private set; }
+        public double SaldoConta { get; private set; }
+
+
+
+        public Conta(string nome, int numeroConta)
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+            SaldoConta = 0;
+        }
+
+        public Conta(string nome, int numeroConta,double saldoConta) : this(nome, numeroConta)
+        {
+            SaldoConta = saldoConta;
+        }
+
+        public double Depositar(double Valor)
+        {
+            return SaldoConta += Valor;
+        }
+        public double Sacar(double saque)
+        {
+            return (SaldoConta -= (saque + 5));
+        }
+
+        public override string ToString()
+        {
+            return $"Titular da conta : {_nome}, Numero da conta : {NumeroConta} com o saldo de R${SaldoConta:f2}";
+        }
+
+
+
+
+    }
+}
---
+++ exercicio banco/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exercicio_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome ?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual o numero da sua conta ?");
+            int nConta = int.Parse(Console.ReadLine());
+
+            Conta conta = new Conta(nome, nConta);
+
+            Console.WriteLine("Haverá depósito inicial ? (s/n)");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            if (depositoInicial == 's')
+            {
+                Console.WriteLine("Qual será o valor do seu depósito ?");
+                double valor = double.Parse(Console.ReadLine());
+                conta.Depositar(valor);
+            }
+
+
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Quanto você gostaria de depositar ? ");
+            conta.Depositar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Quanto você gostaria de sacar ? ");
+            conta.Sacar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta);
+
+
+            
+
+        }
+    }
+}
